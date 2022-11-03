09af8958b04df7fd9ef30779c9856e4102370dc6
samin_batra29 <saminbatra1@gmail.com>
2022-11-03 15:44:53 +0800

    Added functionality to get drivers' championship progression.

--- main.py
+++ main.py
@@ -22,7 +22,6 @@ def get_laptimes(raceId):
         "select l.raceId as ""RaceId"", concat(d.firstname, ' ',d.lastname) as ""Name"", l.lap as ""Lap"",l.lap_position as ""Position"", l.lap_time as ""Time"",l.milliseconds as ""Time_MS"" from lap_times l natural inner join driver d "
         "where l.raceId in (select raceId from race where raceId in (select raceId from results where raceId = '" + raceId + "'));",
         engine)
-    print(race_laptimes.head())
     race_laptimes['time_ms'] = race_laptimes['time_ms'].astype(int)
     race_laptimes['name'] = race_laptimes['name'].astype(str)
     race_laptimes['time_ms'] = race_laptimes['time_ms']/1000
@@ -51,7 +50,6 @@ def get_qualy_comparisons(raceId):
     last_race_grouped = last_race.merge(constructors_df, on="constructorid",how="inner")
     curr_race_grouped = curr_race.merge(constructors_df, on="constructorid",how="inner")
     qualy_gains_df = last_race_grouped.merge(curr_race_grouped,on="constructorid",how="inner")
-    print(qualy_gains_df)
     qualy_gains_df['time_diff'] = qualy_gains_df['avg_y'] - qualy_gains_df['avg_x']
     qualy_gains_df.drop(columns='name_y',inplace=True)
     qualy_gains_df.rename(columns = {'avg_x':'last_race','avg_y':'curr_race','name_x':'name'},inplace=True)
@@ -62,24 +60,17 @@ def get_qualy_comparisons(raceId):
 
 @app.route("/",methods=["GET","POST"])
 def home():
-    # connection = sqlite3.connect("f1.db")
     if request.method=="GET":
         races_2022 = pd.read_sql("select r.raceId, r.name ""Race"", r.circuitname ""Circuit"" from race r where year = 2022 order by r.race_date",engine)
-        print(races_2022)
         return render_template("index.html", race_details=races_2022, display=False)
     elif request.method=="POST":
-        print(request.form)
         raceId = request.form.get('race')
         race_details = pd.read_sql("select r.year ""race_year"", r.name ""Race"", r.circuitname ""Circuit"" from race r where raceId='" + raceId + "';",engine)
-        print(race_details)
         race_results_df = pd.read_sql("select r.positiontext ""Position"", d.firstname || ' ' || d.lastname ""Driver"", c.name ""Team"", r.points ""Points"", r.status ""Status""  from results r natural inner JOIN "
                                       "driver d "  
                                       "natural inner join constructors c where r.raceId = '" + raceId + "' "
                                       "order by r.positionorder asc limit 20;",engine)
-        print(race_results_df)
         race_details = race_details.to_dict()
-        print(race_details['race_year'][0])
-        str()
         lap_data = get_laptimes(raceId)
         qualy_diff = get_qualy_comparisons(raceId)
         return_obj = {
@@ -91,9 +82,48 @@ def home():
         races_2022 = pd.read_sql(
             "select r.raceId, r.name ""Race"", r.circuitname ""Circuit"" from race r where year = 2022 order by r.race_date",
             engine)
-
         return render_template("index.html",races =return_obj,display=True,race_details=races_2022)
 
+@app.route("/drivers-championship",methods=["GET","POST"])
+def driversChampionship():
+    if request.method=="GET":
+        drivers_2022 = pd.read_sql("select d.driverid ""driverid"", d.firstname || ' ' || d.lastname ""drivername""  from driver d where d.driverid in ("
+            "select distinct driverid from results where raceid in (select raceid from race where year=2022))",engine)
+        return render_template("drivers.html", driver_details=drivers_2022, display=False)
+
+    else:
+        driver_1 =  request.form.get('driver_1')
+        driver_2 = request.form.get('driver_2')
+        driver_1_points = pd.read_sql("with data as ("
+    "select driverid,raceid, sum(points) ""points"" from results where raceid in (select raceid from race where year=2022 order by race_date) and driverid in ('" + driver_1 + "')"
+    "group by driverid, raceid"
+    ")"
+    "select "
+    "driverid, round, firstname || ' ' || lastname ""drivername"", "
+    "sum(points) over (order by raceid asc rows between unbounded preceding and current row)"
+    "from data natural join race natural join driver order by round asc",engine)
+
+        driver_2_points = pd.read_sql("with data as ("
+                                      "select driverid,raceid, sum(points) ""points"" from results where raceid in (select raceid from race where year=2022 order by race_date) and driverid in ('" + driver_2 + "')"
+                                     "group by driverid, raceid"
+                                     ")"
+                                    "select "
+                                    "driverid, round, firstname || ' ' || lastname ""drivername"", "
+                                    "sum(points) over (order by raceid asc rows between unbounded preceding and current row)"
+                                    "from data natural join race natural join driver order by round asc",
+                                      engine)
+
+        driver_1_points = driver_1_points.append(driver_2_points)
+        rounds = driver_1_points['round'].unique().tolist()
+        drivers = pd.unique(driver_1_points['drivername'])
+        driver_data = {'label': rounds, 'data': [
+            {'data': driver_1_points.loc[driver_1_points['drivername'] == str(driver), 'sum'].values.tolist(),
+             'label': str(driver), 'borderColor': "#" + ''.join([random.choice('0123456789ABCDEF') for j in range(6)]),
+             'fill': False} for driver in drivers]}
+        drivers_2022 = pd.read_sql(
+            "select d.driverid ""driverid"", d.firstname || ' ' || d.lastname ""drivername""  from driver d where d.driverid in ("
+            "select distinct driverid from results where raceid in (select raceid from race where year=2022))", engine)
+        return render_template("drivers.html",display = True,drivers_data = driver_data, driver_details = drivers_2022)
 
 if __name__=='__main__':
     app.run(debug=True)
